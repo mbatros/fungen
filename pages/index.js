d478c69e274d01e715d9ff737840e488ec351db3
mbatros <info@infinityhq.com.au>
2026-02-23 01:11:33 +1100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import RoastMeButton from "../components/RoastMeButton";
+export default function Home() {
+  return (
+    <div style={{textAlign:"center",padding:"50px"}}>
+      <h1>🔥 Welcome to Roast Me! 🔥</h1>
+      <RoastMeButton/>
+    </div>
+  );
+}
