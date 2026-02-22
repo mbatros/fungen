67458ac7a3ce7bf0da4f417ac51386510d900bc7
mbatros <info@infinityhq.com.au>
2026-02-22 23:42:18 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import RoastMeButton from "../components/RoastMeButton";
 export default function Home() {
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to Roast Me!</h1>
+      <h1>🔥 Welcome to Roast Me! 🔥</h1>
       <RoastMeButton />
     </div>
   );
