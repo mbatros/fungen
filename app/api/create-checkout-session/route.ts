4318df43133bd49826cdee874c878764799035b5
mbatros <info@infinityhq.com.au>
2026-02-25 17:14:13 +1100

    Create route.ts

---
+++ app/api/create-checkout-session/route.ts
@@ -0,0 +1,25 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export async function POST() {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: process.env.STRIPE_PRICE_ID as string,
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+    });
+
+    return NextResponse.json({ url: session.url });
+  } catch (err) {
+    return NextResponse.json({ error: "Stripe error" }, { status: 500 });
+  }
+}
