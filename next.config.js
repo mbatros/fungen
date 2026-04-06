621cc779fc12c5344e16099e4731004030ba571f
mbatros <info@infinityhq.com.au>
2026-04-06 10:34:12 +0000

    Trigger rebuild

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: true,
+  },
+  // ⭐ Prevent Next.js from trying to statically export pages
+  generateStaticParams: () => [],
 };
 
 module.exports = nextConfig;
